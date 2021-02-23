38c4e0c4581fc663bb7b96a8ef86843cbc431ca0
胡奕舟 <739820174@qq.com>
2021-02-23 14:18:07 +0800

    1. 增加eureka子模块作为注册服务端 2. 将单maven项目改造成maven父子项目

---
+++ eureka/src/main/java/cn/edu/zucc/galiview/EurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zucc.galiview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
