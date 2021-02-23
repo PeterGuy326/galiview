6bd56aa92fb869c2242072eeb2dc678f42eeecc9
胡奕舟 <739820174@qq.com>
2021-02-23 13:44:25 +0800

    spring 初始项目

---
+++ src/test/java/cn/edu/zucc/galiview/GaliviewApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zucc.galiview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaliviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
