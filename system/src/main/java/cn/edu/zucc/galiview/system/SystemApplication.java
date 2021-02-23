43468811088524a96819c351e6ca8b0df6a045cf
胡奕舟 <739820174@qq.com>
2021-02-23 15:05:31 +0800

    1. 将system配置成最简单的SpringBoot项目

---
+++ system/src/main/java/cn/edu/zucc/galiview/system/SystemApplication.java
@@ -0,0 +1,21 @@
+package cn.edu.zucc.galiview.system;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class SystemApplication {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(SystemApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("System地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
+}
---
+++ system/src/main/java/cn/edu/zucc/galiview/system/controller/TestController.java
@@ -0,0 +1,13 @@
+package cn.edu.zucc.galiview.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "success";
+    }
+}
