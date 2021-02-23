e3588f0470ea28be169aea62c8bbc6e116dd7018
胡奕舟 <739820174@qq.com>
2021-02-23 15:07:57 +0800

    1. 将system注册到eureka

--- system/src/main/java/cn/edu/zucc/galiview/system/SystemApplication.java
+++ system/src/main/java/cn/edu/zucc/galiview/system/SystemApplication.java
@@ -5,9 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
