2ba70bdfbb6edec5d921a7058bd4e2a7f52b0d0a
胡奕舟 <739820174@qq.com>
2021-02-23 20:22:16 +0800

    1. Example使用示例 使用Example可以快速完成单表的增删查改，熟练掌握后对代码开发效率将有一个质的飞跃

--- server/src/main/java/cn/edu/zucc/galiview/server/service/TestService.java
+++ server/src/main/java/cn/edu/zucc/galiview/server/service/TestService.java
@@ -1,6 +1,7 @@
 package cn.edu.zucc.galiview.server.service;
 
 import cn.edu.zucc.galiview.server.domain.Test;
+import cn.edu.zucc.galiview.server.domain.TestExample;
 import cn.edu.zucc.galiview.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -12,4 +13,11 @@ public class TestService {
 
     @Resource
     private TestMapper testMapper;
+
+    public List<Test> list() {
+        TestExample testExample = new TestExample();
+        testExample.createCriteria().andIdEqualTo("1");
+        testExample.setOrderByClause("id desc");
+        return testMapper.selectByExample(testExample);
+    }
 }
