f26feb883ac05759f0bb2a10ebef2de4d5e436e7
胡奕舟 <739820174@qq.com>
2021-02-23 20:10:57 +0800

    1. 集成mybatis-generator 步骤1：添加idea插件 步骤2：添加server\src\main\resources\generator\generatorConfig.xml 步骤3：创建maven启动命令mybatis-generator:generate -e

--- server/src/main/java/cn/edu/zucc/galiview/server/domain/Test.java
+++ server/src/main/java/cn/edu/zucc/galiview/server/domain/Test.java
@@ -20,4 +20,16 @@ public class Test {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
+    }
 }
--- server/src/main/java/cn/edu/zucc/galiview/server/mapper/TestMapper.java
+++ server/src/main/java/cn/edu/zucc/galiview/server/mapper/TestMapper.java
@@ -1,10 +1,30 @@
 package cn.edu.zucc.galiview.server.mapper;
 
 import cn.edu.zucc.galiview.server.domain.Test;
-
+import cn.edu.zucc.galiview.server.domain.TestExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface TestMapper {
+    long countByExample(TestExample example);
 
-    public List<Test> list();
+    int deleteByExample(TestExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
 }
