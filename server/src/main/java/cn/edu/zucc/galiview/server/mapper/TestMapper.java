c47065d387937cb15ade15be3f9fb0e5459ac4c1
胡奕舟 <739820174@qq.com>
2021-02-23 17:50:26 +0800

    1. 新建公共模块server

---
+++ server/src/main/java/cn/edu/zucc/galiview/server/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package cn.edu.zucc.galiview.server.mapper;
+
+import cn.edu.zucc.galiview.server.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+    public List<Test> list();
+}
---
+++ server/src/main/java/cn/edu/zucc/galiview/server/service/TestService.java
@@ -0,0 +1,19 @@
+package cn.edu.zucc.galiview.server.service;
+
+import cn.edu.zucc.galiview.server.domain.Test;
+import cn.edu.zucc.galiview.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list() {
+        return testMapper.list();
+    }
+}
