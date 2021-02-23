4bcc033e859cc3898fc5ebb633754154991b3aac
胡奕舟 <739820174@qq.com>
2021-02-23 14:38:23 +0800

    1. 解决注册中心服务启动失败的问题 2. 优化启动日志显示

--- eureka/src/main/java/cn/edu/zucc/galiview/EurekaApplication.java
+++ eureka/src/main/java/cn/edu/zucc/galiview/EurekaApplication.java
@@ -1,13 +1,27 @@
 package cn.edu.zucc.galiview;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
 
+//	public static void main(String[] args) {
+//		SpringApplication.run(EurekaApplication.class, args);
+//	}
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
 }
