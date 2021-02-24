e7b030a800c7603a1df9a5a0044f85ad8f86d2c0
胡奕舟 <739820174@qq.com>
2021-02-24 16:22:54 +0800

    1. 侧边栏去掉无用的菜单 2. 底部文案修改

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -16,7 +16,7 @@
           <a href="index.html" class="navbar-brand">
             <small>
               <i class="fa fa-leaf"></i>
-              在线视频课程
+              动漫游戏运营社区平台
             </small>
           </a>
         </div>
@@ -356,214 +356,16 @@
           <li class="">
             <a href="index.html">
               <i class="menu-icon fa fa-tachometer"></i>
-              <span class="menu-text"> Dashboard </span>
+              <span class="menu-text"> 欢迎 </span>
             </a>
 
             <b class="arrow"></b>
           </li>
 
-          <li class="">
-            <a href="#" class="dropdown-toggle">
-              <i class="menu-icon fa fa-desktop"></i>
-              <span class="menu-text">
-								UI &amp; Elements
-							</span>
-
-              <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-              <li class="">
-                <a href="#" class="dropdown-toggle">
-                  <i class="menu-icon fa fa-caret-right"></i>
-
-                  Layouts
-                  <b class="arrow fa fa-angle-down"></b>
-                </a>
-
-                <b class="arrow"></b>
-
-                <ul class="submenu">
-                  <li class="">
-                    <a href="top-menu.html">
-                      <i class="menu-icon fa fa-caret-right"></i>
-                      Top Menu
-                    </a>
-
-                    <b class="arrow"></b>
-                  </li>
-
-                  <li class="">
-                    <a href="two-menu-1.html">
-                      <i class="menu-icon fa fa-caret-right"></i>
-                      Two Menus 1
-                    </a>
-
-                    <b class="arrow"></b>
-                  </li>
-
-                  <li class="">
-                    <a href="two-menu-2.html">
-                      <i class="menu-icon fa fa-caret-right"></i>
-                      Two Menus 2
-                    </a>
-
-                    <b class="arrow"></b>
-                  </li>
-
-                  <li class="">
-                    <a href="mobile-menu-1.html">
-                      <i class="menu-icon fa fa-caret-right"></i>
-                      Default Mobile Menu
-                    </a>
-
-                    <b class="arrow"></b>
-                  </li>
-
-                  <li class="">
-                    <a href="mobile-menu-2.html">
-                      <i class="menu-icon fa fa-caret-right"></i>
-                      Mobile Menu 2
-                    </a>
-
-                    <b class="arrow"></b>
-                  </li>
-
-                  <li class="">
-                    <a href="mobile-menu-3.html">
-                      <i class="menu-icon fa fa-caret-right"></i>
-                      Mobile Menu 3
-                    </a>
-
-                    <b class="arrow"></b>
-                  </li>
-                </ul>
-              </li>
-
-              <li class="">
-                <a href="typography.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Typography
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="elements.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Elements
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="buttons.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Buttons &amp; Icons
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="content-slider.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Content Sliders
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="treeview.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Treeview
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="jquery-ui.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  jQuery UI
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="nestable-list.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Nestable Lists
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="#" class="dropdown-toggle">
-                  <i class="menu-icon fa fa-caret-right"></i>
-
-                  Three Level Menu
-                  <b class="arrow fa fa-angle-down"></b>
-                </a>
-
-                <b class="arrow"></b>
-
-                <ul class="submenu">
-                  <li class="">
-                    <a href="#">
-                      <i class="menu-icon fa fa-leaf green"></i>
-                      Item #1
-                    </a>
-
-                    <b class="arrow"></b>
-                  </li>
-
-                  <li class="">
-                    <a href="#" class="dropdown-toggle">
-                      <i class="menu-icon fa fa-pencil orange"></i>
-
-                      4th level
-                      <b class="arrow fa fa-angle-down"></b>
-                    </a>
-
-                    <b class="arrow"></b>
-
-                    <ul class="submenu">
-                      <li class="">
-                        <a href="#">
-                          <i class="menu-icon fa fa-plus purple"></i>
-                          Add Product
-                        </a>
-
-                        <b class="arrow"></b>
-                      </li>
-
-                      <li class="">
-                        <a href="#">
-                          <i class="menu-icon fa fa-eye pink"></i>
-                          View Products
-                        </a>
-
-                        <b class="arrow"></b>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-
-          <li class="">
+          <li class="active open">
             <a href="#" class="dropdown-toggle">
               <i class="menu-icon fa fa-list"></i>
-              <span class="menu-text"> Tables </span>
+              <span class="menu-text"> 系统管理 </span>
 
               <b class="arrow fa fa-angle-down"></b>
             </a>
@@ -574,7 +376,7 @@
               <li class="">
                 <a href="tables.html">
                   <i class="menu-icon fa fa-caret-right"></i>
-                  Simple &amp; Dynamic
+                  用户管理
                 </a>
 
                 <b class="arrow"></b>
@@ -583,7 +385,7 @@
               <li class="">
                 <a href="jqgrid.html">
                   <i class="menu-icon fa fa-caret-right"></i>
-                  jqGrid plugin
+                  权限管理
                 </a>
 
                 <b class="arrow"></b>
@@ -591,245 +393,6 @@
             </ul>
           </li>
 
-          <li class="">
-            <a href="#" class="dropdown-toggle">
-              <i class="menu-icon fa fa-pencil-square-o"></i>
-              <span class="menu-text"> Forms </span>
-
-              <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-              <li class="">
-                <a href="form-elements.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Form Elements
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="form-elements-2.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Form Elements 2
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="form-wizard.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Wizard &amp; Validation
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="wysiwyg.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Wysiwyg &amp; Markdown
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="dropzone.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Dropzone File Upload
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-            </ul>
-          </li>
-
-          <li class="">
-            <a href="widgets.html">
-              <i class="menu-icon fa fa-list-alt"></i>
-              <span class="menu-text"> Widgets </span>
-            </a>
-
-            <b class="arrow"></b>
-          </li>
-
-          <li class="">
-            <a href="calendar.html">
-              <i class="menu-icon fa fa-calendar"></i>
-
-              <span class="menu-text">
-								Calendar
-
-								<span class="badge badge-transparent tooltip-error" title="2 Important Events">
-									<i class="ace-icon fa fa-exclamation-triangle red bigger-130"></i>
-								</span>
-							</span>
-            </a>
-
-            <b class="arrow"></b>
-          </li>
-
-          <li class="">
-            <a href="gallery.html">
-              <i class="menu-icon fa fa-picture-o"></i>
-              <span class="menu-text"> Gallery </span>
-            </a>
-
-            <b class="arrow"></b>
-          </li>
-
-          <li class="">
-            <a href="#" class="dropdown-toggle">
-              <i class="menu-icon fa fa-tag"></i>
-              <span class="menu-text"> More Pages </span>
-
-              <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-              <li class="">
-                <a href="profile.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  User Profile
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="inbox.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Inbox
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="pricing.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Pricing Tables
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="invoice.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Invoice
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="timeline.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Timeline
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="search.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Search Results
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="email.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Email Templates
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="login.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Login &amp; Register
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-            </ul>
-          </li>
-
-          <li class="active open">
-            <a href="#" class="dropdown-toggle">
-              <i class="menu-icon fa fa-file-o"></i>
-
-              <span class="menu-text">
-								Other Pages
-
-								<span class="badge badge-primary">5</span>
-							</span>
-
-              <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-              <li class="">
-                <a href="faq.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  FAQ
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="error-404.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Error 404
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="error-500.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Error 500
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="">
-                <a href="grid.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Grid
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-
-              <li class="active">
-                <a href="blank.html">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  Blank Page
-                </a>
-
-                <b class="arrow"></b>
-              </li>
-            </ul>
-          </li>
         </ul><!-- /.nav-list -->
 
         <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
@@ -855,8 +418,8 @@
         <div class="footer-inner">
           <div class="footer-content">
 						<span class="bigger-120">
-							<span class="blue bolder">Ace</span>
-							Application &copy; 2013-2014
+							<span class="blue bolder">huyizhou</span>
+							动漫游戏运营社区平台 &copy; 2017-2099
 						</span>
 
             &nbsp; &nbsp;
@@ -892,6 +455,7 @@ export default {
   mounted: function() {
     $("body").removeClass("login-layout light-login");
     $("body").attr("class", "no-skin");
+    // console.log("admin");
   },
   methods: {
     login () {
