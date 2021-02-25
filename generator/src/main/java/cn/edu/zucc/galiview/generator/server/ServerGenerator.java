95cb89e33bd1d06af24ce32a4568b77effa7c5c1
胡奕舟 <739820174@qq.com>
2021-02-25 17:18:18 +0800

    1. 生成器开发：代码生成器和mybatis generator整合

--- generator/src/main/java/cn/edu/zucc/galiview/generator/server/ServerGenerator.java
+++ generator/src/main/java/cn/edu/zucc/galiview/generator/server/ServerGenerator.java
@@ -3,7 +3,11 @@ package cn.edu.zucc.galiview.generator.server;
 import cn.edu.zucc.galiview.generator.util.DbUtil;
 import cn.edu.zucc.galiview.generator.util.Field;
 import cn.edu.zucc.galiview.generator.util.FreemarkerUtil;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import java.io.File;
 import java.util.*;
 
 public class ServerGenerator {
@@ -11,12 +15,34 @@ public class ServerGenerator {
     static String toDtoPath = "server//src//main//java//cn//edu//zucc//galiview//server//dto//";
     static String toServicePath = "server//src//main//java//cn//edu//zucc//galiview//server//service//";
     static String toControllerPath = MODULE + "//src//main//java//cn//edu//zucc//galiview//" + MODULE + "//controller//admin//";
+    static String generatorConfigPath = "server//src//main//resources//generator//generatorConfig.xml";
 
     public static void main(String[] args) throws Exception {
-        String Domain = "Section";
-        String domain = "section";
-        String tableNameCn = "小节";
         String module = MODULE;
+
+
+        // 只生成配置文件中的第一个table节点
+        File file = new File(generatorConfigPath);
+        SAXReader reader=new SAXReader();
+        //读取xml文件到Document中
+        Document doc=reader.read(file);
+        //获取xml文件的根节点
+        Element rootElement=doc.getRootElement();
+        //读取context节点
+        Element contextElement = rootElement.element("context");
+        //定义一个Element用于遍历
+        Element tableElement;
+        //取第一个“table”的节点
+        tableElement=contextElement.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName");
+        String tableName = tableElement.attributeValue("tableName");
+        String tableNameCn = DbUtil.getTableComment(tableName);
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        System.out.println("表："+tableElement.attributeValue("tableName"));
+        System.out.println("Domain："+tableElement.attributeValue("domainObjectName"));
+
+
+
         List<Field> fieldList = DbUtil.getColumnByTableName(domain);
         Set<String> typeSet = getJavaTypes(fieldList);
         Map<String, Object> map = new HashMap<>();
