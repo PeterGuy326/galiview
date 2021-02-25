dfad3168a6fcf007439169c2776a0497deacf1d4
胡奕舟 <739820174@qq.com>
2021-02-25 15:26:01 +0800

    1. 修改Toastr组件的显示效果，更大气 2. 制作Confirm组件

--- admin/public/static/js/toast.js
+++ admin/public/static/js/toast.js
@@ -1,34 +1,31 @@
-const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    onOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-});
-
-toast = {
+Toast = {
     success: function (message) {
-        Toast.fire({
+        Swal.fire({
+            position: 'top-end',
             icon: 'success',
-            title: message
-        });
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
+        })
     },
 
     error: function (message) {
-        Toast.fire({
+        Swal.fire({
+            position: 'top-end',
             icon: 'error',
-            title: message
-        });
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
+        })
     },
 
     warning: function (message) {
-        Toast.fire({
+        Swal.fire({
+            position: 'top-end',
             icon: 'warning',
-            title: message
-        });
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
+        })
     }
 };
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -134,36 +134,49 @@ export default {
         if (resp.success) {
           $("#form-modal").modal("hide");
           _this.list(1);
-          toast.success("保存成功！");
+          Toast.success("保存成功！");
         }
       })
     },
 
     del(id) {
       let _this = this;
-      Swal.fire({
-        title: '确认删除？',
-        text: "删除后不可恢复，确认删除？",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: '确认!'
-      }).then((result) => {
-        if (result.value) {
-          Loading.show();
-          _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response)=>{
-            Loading.hide();
-            console.log("删除大章列表结果：", response);
-            let resp = response.data;
-            if (resp.success) {
-              _this.list(1);
-              toast.success("删除成功！");
-            }
-          })
+      Confirm.show("删除大章后不可恢复，确认删除？", function () {
+        Loading.show();
+        _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response)=>{
+          Loading.hide();
+          console.log("删除大章列表结果：", response);
+          let resp = response.data;
+          if (resp.success) {
+            _this.list(1);
+            Toast.success("删除成功！");
+          }
+        })
+      });
 
-        }
-      })
+      // Swal.fire({
+      //   title: '确认删除？',
+      //   text: "删除后不可恢复，确认删除？",
+      //   icon: 'warning',
+      //   showCancelButton: true,
+      //   confirmButtonColor: '#3085d6',
+      //   cancelButtonColor: '#d33',
+      //   confirmButtonText: '确认!'
+      // }).then((result) => {
+      //   if (result.value) {
+      //     Loading.show();
+      //     _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response)=>{
+      //       Loading.hide();
+      //       console.log("删除大章列表结果：", response);
+      //       let resp = response.data;
+      //       if (resp.success) {
+      //         _this.list(1);
+      //         Toast.success("删除成功！");
+      //       }
+      //     })
+      //
+      //   }
+      // })
 
     }
   }
