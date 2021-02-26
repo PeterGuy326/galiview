c22ef528e8c23a5a8d291d1e52758d6f31238727
胡奕舟 <739820174@qq.com>
2021-02-26 17:58:11 +0800

    1. ztree数据改为分类所有记录

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -169,12 +169,13 @@ export default {
       COURSE_LEVEL: COURSE_LEVEL,
       COURSE_CHARGE: COURSE_CHARGE,
       COURSE_STATUS: COURSE_STATUS,
+      categorys: [],
     }
   },
   mounted: function () {
     let _this = this;
     _this.$refs.pagination.size = 5;
-    _this.initTree();
+    _this.allCategory();
     _this.list(1);
     // sidebar激活样式方法一
     // this.$parent.activeSidebar("business-course-sidebar");
@@ -274,33 +275,35 @@ export default {
       _this.$router.push("/business/chapter");
     },
 
+    allCategory() {
+      let _this = this;
+      Loading.show();
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/category/all').then((response) => {
+        Loading.hide();
+        let resp = response.data;
+        _this.categorys = resp.content;
+
+        _this.initTree();
+      })
+    },
+
     initTree() {
+      let _this = this;
       let setting = {
         check: {
           enable: true
         },
         data: {
           simpleData: {
+            idKey: "id",
+            pIdKey: "parent",
+            rootPId: "00000000",
             enable: true
           }
         }
       };
 
-      let zNodes = [
-        {id: 1, pId: 0, name: "随意勾选 1", open: true},
-        {id: 11, pId: 1, name: "随意勾选 1-1", open: true},
-        {id: 111, pId: 11, name: "随意勾选 1-1-1"},
-        {id: 112, pId: 11, name: "随意勾选 1-1-2"},
-        {id: 12, pId: 1, name: "随意勾选 1-2", open: true},
-        {id: 121, pId: 12, name: "随意勾选 1-2-1"},
-        {id: 122, pId: 12, name: "随意勾选 1-2-2"},
-        {id: 2, pId: 0, name: "随意勾选 2", checked: true, open: true},
-        {id: 21, pId: 2, name: "随意勾选 2-1"},
-        {id: 22, pId: 2, name: "随意勾选 2-2", open: true},
-        {id: 221, pId: 22, name: "随意勾选 2-2-1", checked: true},
-        {id: 222, pId: 22, name: "随意勾选 2-2-2"},
-        {id: 23, pId: 2, name: "随意勾选 2-3"}
-      ];
+      let zNodes = _this.categorys;
 
       $.fn.zTree.init($("#tree"), setting, zNodes);
     }
