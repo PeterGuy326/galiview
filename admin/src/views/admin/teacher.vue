559f884087877f0586b1126a8c95bc8e56ccb7ab
胡奕舟 <739820174@qq.com>
2021-02-28 19:26:18 +0800

    1. 讲师管理页面布局优化

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -14,42 +14,50 @@
 
     <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
 
-    <table id="simple-table" class="table  table-bordered table-hover">
-      <thead>
-      <tr>
-                    <th>id</th>
-            <th>姓名</th>
-            <th>昵称</th>
-            <th>头像</th>
-            <th>职位</th>
-            <th>座右铭</th>
-            <th>简介</th>
-        <th>操作</th>
-      </tr>
-      </thead>
+    <div class="row">
+      <div v-for="teacher in teachers" class="col-md-3 center">
+        <div>
+          <span class="profile-picture">
+            <img v-show="!teacher.image" class="editable img-responsive editable-click editable-empty" src="/ace/assets/images/avatars/profile-pic.jpg" v-bind:title="teacher.intro"/>
+            <img v-show="teacher.image" class="media-object" v-bind:src="teacher.image" v-bind:title="teacher.intro"/>
+          </span>
 
-      <tbody>
-      <tr v-for="teacher in teachers">
-              <td>{{teacher.id}}</td>
-              <td>{{teacher.name}}</td>
-              <td>{{teacher.nickname}}</td>
-              <td>{{teacher.image}}</td>
-              <td>{{teacher.position}}</td>
-              <td>{{teacher.motto}}</td>
-              <td>{{teacher.intro}}</td>
-        <td>
-          <div class="hidden-sm hidden-xs btn-group">
-            <button v-on:click="edit(teacher)" class="btn btn-xs btn-info">
-              <i class="ace-icon fa fa-pencil bigger-120"></i>
-            </button>
-            <button v-on:click="del(teacher.id)" class="btn btn-xs btn-danger">
-              <i class="ace-icon fa fa-trash-o bigger-120"></i>
-            </button>
+          <div class="space-4"></div>
+
+          <div class="width-85 label label-info label-xlg arrowed-in arrowed-in-right">
+            <div class="inline position-relative">
+              <a href="javascript:;" class="user-title-label dropdown-toggle" data-toggle="dropdown">
+                <i class="ace-icon fa fa-circle light-green"></i>
+                &nbsp;
+                <span class="white">{{teacher.position}}</span>
+              </a>
+            </div>
           </div>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+        </div>
+
+        <div class="space-6"></div>
+
+        <a href="javascript:;" class="text-info bigger-110" v-bind:title="teacher.motto">
+          <i class="ace-icon fa fa-user"></i>
+          {{teacher.name}}【{{teacher.nickname}}】
+        </a>
+
+        <div class="space-6"></div>
+
+        <div class="profile-social-links align-center">
+          <button v-on:click="edit(teacher)" class="btn btn-xs btn-info">
+            <i class="ace-icon fa fa-pencil bigger-120"></i>
+          </button>
+          &nbsp;
+          <button v-on:click="del(teacher.id)" class="btn btn-xs btn-danger">
+            <i class="ace-icon fa fa-trash-o bigger-120"></i>
+          </button>
+        </div>
+
+        <div class="hr hr16 dotted"></div>
+
+      </div>
+    </div>
 
     <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
@@ -60,42 +68,42 @@
           </div>
           <div class="modal-body">
             <form class="form-horizontal">
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">姓名</label>
-                      <div class="col-sm-10">
-                        <input v-model="teacher.name" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">昵称</label>
-                      <div class="col-sm-10">
-                        <input v-model="teacher.nickname" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">头像</label>
-                      <div class="col-sm-10">
-                        <input v-model="teacher.image" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">职位</label>
-                      <div class="col-sm-10">
-                        <input v-model="teacher.position" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">座右铭</label>
-                      <div class="col-sm-10">
-                        <input v-model="teacher.motto" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">简介</label>
-                      <div class="col-sm-10">
-                        <input v-model="teacher.intro" class="form-control">
-                      </div>
-                    </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">姓名</label>
+                <div class="col-sm-10">
+                  <input v-model="teacher.name" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">昵称</label>
+                <div class="col-sm-10">
+                  <input v-model="teacher.nickname" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">头像</label>
+                <div class="col-sm-10">
+                  <input v-model="teacher.image" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">职位</label>
+                <div class="col-sm-10">
+                  <input v-model="teacher.position" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">座右铭</label>
+                <div class="col-sm-10">
+                  <input v-model="teacher.motto" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">简介</label>
+                <div class="col-sm-10">
+                  <textarea v-model="teacher.intro" class="form-control" rows="5"></textarea>
+                </div>
+              </div>
             </form>
           </div>
           <div class="modal-footer">
@@ -109,111 +117,111 @@
 </template>
 
 <script>
-  import Pagination from "../../components/pagination";
-  export default {
-    components: {Pagination},
-    name: "business-teacher",
-    data: function() {
-      return {
-        teacher: {},
-        teachers: [],
-      }
-    },
-    mounted: function() {
+import Pagination from "../../components/pagination";
+export default {
+  components: {Pagination},
+  name: "business-teacher",
+  data: function() {
+    return {
+      teacher: {},
+      teachers: [],
+    }
+  },
+  mounted: function() {
+    let _this = this;
+    _this.$refs.pagination.size = 5;
+    _this.list(1);
+    // sidebar激活样式方法一
+    // this.$parent.activeSidebar("business-teacher-sidebar");
+
+  },
+  methods: {
+    /**
+     * 点击【新增】
+     */
+    add() {
       let _this = this;
-      _this.$refs.pagination.size = 5;
-      _this.list(1);
-      // sidebar激活样式方法一
-      // this.$parent.activeSidebar("business-teacher-sidebar");
-
+      _this.teacher = {};
+      $("#form-modal").modal("show");
     },
-    methods: {
-      /**
-       * 点击【新增】
-       */
-      add() {
-        let _this = this;
-        _this.teacher = {};
-        $("#form-modal").modal("show");
-      },
 
-      /**
-       * 点击【编辑】
-       */
-      edit(teacher) {
-        let _this = this;
-        _this.teacher = $.extend({}, teacher);
-        $("#form-modal").modal("show");
-      },
+    /**
+     * 点击【编辑】
+     */
+    edit(teacher) {
+      let _this = this;
+      _this.teacher = $.extend({}, teacher);
+      $("#form-modal").modal("show");
+    },
 
-      /**
-       * 列表查询
-       */
-      list(page) {
-        let _this = this;
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/teacher/list', {
-          page: page,
-          size: _this.$refs.pagination.size,
-        }).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          _this.teachers = resp.content.list;
-          _this.$refs.pagination.render(page, resp.content.total);
+    /**
+     * 列表查询
+     */
+    list(page) {
+      let _this = this;
+      Loading.show();
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/teacher/list', {
+        page: page,
+        size: _this.$refs.pagination.size,
+      }).then((response)=>{
+        Loading.hide();
+        let resp = response.data;
+        _this.teachers = resp.content.list;
+        _this.$refs.pagination.render(page, resp.content.total);
 
-        })
-      },
+      })
+    },
 
-      /**
-       * 点击【保存】
-       */
-      save() {
-        let _this = this;
+    /**
+     * 点击【保存】
+     */
+    save() {
+      let _this = this;
 
-        // 保存校验
-        if (1 != 1
-                || !Validator.require(_this.teacher.name, "姓名")
-                || !Validator.length(_this.teacher.name, "姓名", 1, 50)
-                || !Validator.length(_this.teacher.nickname, "昵称", 1, 50)
-                || !Validator.length(_this.teacher.image, "头像", 1, 100)
-                || !Validator.length(_this.teacher.position, "职位", 1, 50)
-                || !Validator.length(_this.teacher.motto, "座右铭", 1, 50)
-                || !Validator.length(_this.teacher.intro, "简介", 1, 500)
-        ) {
-          return;
+      // 保存校验
+      if (1 != 1
+          || !Validator.require(_this.teacher.name, "姓名")
+          || !Validator.length(_this.teacher.name, "姓名", 1, 50)
+          || !Validator.length(_this.teacher.nickname, "昵称", 1, 50)
+          || !Validator.length(_this.teacher.image, "头像", 1, 100)
+          || !Validator.length(_this.teacher.position, "职位", 1, 50)
+          || !Validator.length(_this.teacher.motto, "座右铭", 1, 50)
+          || !Validator.length(_this.teacher.intro, "简介", 1, 500)
+      ) {
+        return;
+      }
+
+      Loading.show();
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/teacher/save', _this.teacher).then((response)=>{
+        Loading.hide();
+        let resp = response.data;
+        if (resp.success) {
+          $("#form-modal").modal("hide");
+          _this.list(1);
+          Toast.success("保存成功！");
+        } else {
+          Toast.warning(resp.message)
         }
+      })
+    },
 
+    /**
+     * 点击【删除】
+     */
+    del(id) {
+      let _this = this;
+      Confirm.show("删除讲师后不可恢复，确认删除？", function () {
         Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/teacher/save', _this.teacher).then((response)=>{
+        _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/teacher/delete/' + id).then((response)=>{
           Loading.hide();
           let resp = response.data;
           if (resp.success) {
-            $("#form-modal").modal("hide");
             _this.list(1);
-            Toast.success("保存成功！");
-          } else {
-            Toast.warning(resp.message)
+            Toast.success("删除成功！");
           }
         })
-      },
-
-      /**
-       * 点击【删除】
-       */
-      del(id) {
-        let _this = this;
-        Confirm.show("删除讲师后不可恢复，确认删除？", function () {
-          Loading.show();
-          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/teacher/delete/' + id).then((response)=>{
-            Loading.hide();
-            let resp = response.data;
-            if (resp.success) {
-              _this.list(1);
-              Toast.success("删除成功！");
-            }
-          })
-        });
-      }
+      });
     }
   }
+}
 </script>
