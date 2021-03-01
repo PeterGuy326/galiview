9512c718f2ce6304c1acab6bd9f02f7d3643a3fb
胡奕舟 <739820174@qq.com>
2021-03-01 20:45:32 +0800

    1. 文件上传功能开发：增加上传文件类型的判断

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -87,7 +87,7 @@
                     <i class="ace-icon fa fa-upload"></i>
                     上传头像
                   </button>
-                  <input class="hidden" type="file" v-on:change="uploadImage()" id="file-upload-input">
+                  <input class="hidden" type="file" ref="file" v-on:change="uploadImage()" id="file-upload-input">
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
                       <img v-bind:src="teacher.image" class="img-responsive">
@@ -235,8 +235,26 @@ export default {
     uploadImage () {
       let _this = this;
       let formData = new window.FormData();
+      let file = _this.$refs.file.files[0];
+
+      // 判断文件格式
+      let suffixs = ["jpg", "jpeg", "png"];
+      let fileName = file.name;
+      let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
+      let validateSuffix = false;
+      for (let i = 0; i < suffixs.length; i++) {
+        if (suffixs[i].toLowerCase() === suffix) {
+          validateSuffix = true;
+          break;
+        }
+      }
+      if (!validateSuffix) {
+        Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
+        return;
+      }
+
       // key："file"必须和后端controller参数名一致
-      formData.append('file', document.querySelector('#file-upload-input').files[0]);
+      formData.append('file', file);
       Loading.show();
       _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then((response)=>{
         Loading.hide();
