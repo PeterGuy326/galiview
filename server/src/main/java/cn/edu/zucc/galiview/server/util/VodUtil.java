3eb4f0fc8c19f38517c5850cbd9f8d99dc38bcc4
胡奕舟 <739820174@qq.com>
2021-03-23 20:39:27 +0800

    1. 视频加密与授权播放：基于OSS原生SDK上传视频到点播服务，视频点播不支持追加上传

--- server/src/main/java/cn/edu/zucc/galiview/server/util/VodUtil.java
+++ server/src/main/java/cn/edu/zucc/galiview/server/util/VodUtil.java
@@ -79,7 +79,16 @@ public class VodUtil {
         String bucketName = uploadAddress.getString("Bucket");
         String objectName = uploadAddress.getString("FileName");
         File file = new File(localFile);
+        // 单文件上传
         ossClient.putObject(bucketName, objectName, file);
+
+        /* 视频点播不支持追加上传
+        // 追加上传
+        ObjectMetadata meta = new ObjectMetadata();
+        meta.setContentType("text/plain");
+        AppendObjectRequest request = new AppendObjectRequest(bucketName, objectName, file, meta);
+        request.setPosition(0L);
+        ossClient.appendObject(request);*/
     }
 
     /**
