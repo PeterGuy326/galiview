1c7459a6c3ec8f4b64968cb3b415d6391eba4c01
胡奕舟 <739820174@qq.com>
2021-03-27 21:56:13 +0800

    1. 用户管理与登录：基于Vue路由的登录拦截

--- admin/src/main.js
+++ admin/src/main.js
@@ -27,6 +27,24 @@ Object.keys(filter).forEach(key => {
   Vue.filter(key, filter[key])
 });
 
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    return item.meta.loginRequire
+  })) {
+    let loginUser = Tool.getLoginUser();
+    if (Tool.isEmpty(loginUser)) {
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+
 new Vue({
   router,
   render: h => h(App),
--- admin/src/router.js
+++ admin/src/router.js
@@ -30,6 +30,9 @@ export default new Router({
         path: "/",
         name: "admin",
         component: Admin,
+        meta: {
+            loginRequire: true
+        },
         children: [{
             path: "welcome",
             name: "welcome",
