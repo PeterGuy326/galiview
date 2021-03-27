2757d4926fc2b7bd82464adfd551446bea081a1f
胡奕舟 <739820174@qq.com>
2021-03-27 23:05:37 +0800

    1. 用户管理与登录：gateway实现控台登录拦截功能

--- gateway/src/main/java/cn/edu/zucc/galiview/gateway/filter/LoginAdminGatewayFilter.java
+++ gateway/src/main/java/cn/edu/zucc/galiview/gateway/filter/LoginAdminGatewayFilter.java
@@ -5,21 +5,53 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
 
 @Component
 public class LoginAdminGatewayFilter implements GatewayFilter, Ordered {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoginAdminGatewayFilter.class);
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+        // 请求地址中不包含/admin/的，不是控台请求，不需要拦截
+        if (!path.contains("/admin/")) {
+            return chain.filter(exchange);
+        }
+        if (path.contains("/system/admin/user/login")
+                || path.contains("/system/admin/user/logout")
+                || path.contains("/system/admin/kaptcha")) {
+            LOG.info("不需要控台登录验证：{}", path);
+            return chain.filter(exchange);
+        }
         //获取header的token参数
         String token = exchange.getRequest().getHeaders().getFirst("token");
-        LOG.info("登录拦截开始，token：{}", token);
-        return chain.filter(exchange);
+        LOG.info("控台登录验证开始，token：{}", token);
+        if (token == null || token.isEmpty()) {
+            LOG.info( "token为空，请求被拦截" );
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        Object object = redisTemplate.opsForValue().get(token);
+        if (object == null) {
+            LOG.warn( "token无效，请求被拦截" );
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        } else {
+            LOG.info("已登录：{}", object);
+            return chain.filter(exchange);
+        }
     }
 
     @Override
