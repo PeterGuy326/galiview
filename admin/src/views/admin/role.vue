578fd9638b4bb7b86ab149b7df693bac14bf8175
胡奕舟 <739820174@qq.com>
2021-03-28 12:44:01 +0800

    1. 通用权限管理：点击【关联用户】按钮时，加载所有用户，弹出角色用户关联模态框

--- admin/src/views/admin/role.vue
+++ admin/src/views/admin/role.vue
@@ -31,6 +31,9 @@
         <td>{{role.desc}}</td>
         <td>
           <div class="hidden-sm hidden-xs btn-group">
+            <button v-on:click="editUser(role)" class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-user bigger-120"></i>
+            </button>
             <button v-on:click="editResource(role)" class="btn btn-xs btn-info">
               <i class="ace-icon fa fa-list bigger-120"></i>
             </button>
@@ -101,6 +104,60 @@
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+
+    <!-- 角色用户关联配置 -->
+    <div id="user-modal" class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">角色用户关联配置</h4>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-md-6">
+                <table id="user-table" class="table table-hover">
+                  <tbody>
+                  <tr v-for="user in users">
+                    <td>{{user.loginName}}</td>
+                    <td class="text-right">
+                      <a href="javascript:;" class="">
+                        <i class="ace-icon fa fa-arrow-circle-right blue"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="col-md-6">
+                <table id="role-user-table" class="table table-hover">
+                  <tbody>
+                  <tr v-for="user in roleUsers">
+                    <td>{{user.loginName}}</td>
+                    <td class="text-right">
+                      <a href="javascript:;" class="">
+                        <i class="ace-icon fa fa-trash blue"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-white btn-default btn-round" data-dismiss="modal">
+              <i class="ace-icon fa fa-times"></i>
+              关闭
+            </button>
+            <button type="button" class="btn btn-white btn-info btn-round" v-on:click="saveUser()">
+              <i class="ace-icon fa fa-plus blue"></i>
+              保存
+            </button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
   </div>
 </template>
 
@@ -115,6 +172,8 @@ export default {
       roles: [],
       resources: [],
       zTree: {},
+      users: [],
+      roleUsers: []
     }
   },
   mounted: function() {
@@ -302,6 +361,35 @@ export default {
           _this.zTree.checkNode(node, true);
         }
       });
+    },
+
+    /**
+     * 点击【用户】
+     */
+    editUser(role) {
+      let _this = this;
+      _this.role = $.extend({}, role);
+      _this.listUser();
+      $("#user-modal").modal("show");
+    },
+
+    /**
+     * 查询所有用户
+     */
+    listUser() {
+      let _this = this;
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/list', {
+        page: 1,
+        size: 9999
+      }).then((response)=>{
+        let resp = response.data;
+        if (resp.success) {
+          _this.users = resp.content.list;
+          console.log(_this.users)
+        } else {
+          Toast.warning(resp.message);
+        }
+      })
     },
   }
 }
