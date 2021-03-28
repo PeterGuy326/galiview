3b635caf2826d80fa21d34846cad4c6407c8b8fa
胡奕舟 <739820174@qq.com>
2021-03-28 12:17:46 +0800

    1. 通用权限管理：增加角色资源关联表，生成持久层和服务端代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -340,6 +340,35 @@ values ('00000001', '开发', '维护资源');
 insert into `role`
 values ('00000002', '业务管理员', '负责业务管理');
 
+drop table if exists `role_resource`;
+create table `role_resource`
+(
+    `id`          char(8) not null default '' comment 'id',
+    `role_id`     char(8) not null comment '角色|id',
+    `resource_id` char(6) not null comment '资源|id',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='角色资源关联';
+
+insert into `role_resource`
+values ('00000000', '00000000', '01');
+insert into `role_resource`
+values ('00000001', '00000000', '0101');
+insert into `role_resource`
+values ('00000002', '00000000', '010101');
+insert into `role_resource`
+values ('00000003', '00000000', '010102');
+insert into `role_resource`
+values ('00000004', '00000000', '010103');
+insert into `role_resource`
+values ('00000005', '00000000', '0102');
+insert into `role_resource`
+values ('00000006', '00000000', '010201');
+insert into `role_resource`
+values ('00000007', '00000000', '0103');
+insert into `role_resource`
+values ('00000008', '00000000', '010301');
+
 #测试
 drop table if exists `test`;
 create table `test`
