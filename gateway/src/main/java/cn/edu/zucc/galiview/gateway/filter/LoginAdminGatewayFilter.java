c9f657cd279bb857c821ccafd042c9d03b34ad36
胡奕舟 <739820174@qq.com>
2021-03-29 20:55:57 +0800

    1. 通用权限管理：增加后端接口权限拦截

--- gateway/src/main/java/cn/edu/zucc/galiview/gateway/filter/LoginAdminGatewayFilter.java
+++ gateway/src/main/java/cn/edu/zucc/galiview/gateway/filter/LoginAdminGatewayFilter.java
@@ -1,5 +1,8 @@
 package cn.edu.zucc.galiview.gateway.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
@@ -50,6 +53,28 @@ public class LoginAdminGatewayFilter implements GatewayFilter, Ordered {
             return exchange.getResponse().setComplete();
         } else {
             LOG.info("已登录：{}", object);
+
+            // 增加权限校验，gateway里没有LoginUserDto，所以全部用JSON操作
+            LOG.info("接口权限校验，请求地址：{}", path);
+            boolean exist = false;
+            JSONObject loginUserDto = JSON.parseObject(String.valueOf(object));
+            JSONArray requests = loginUserDto.getJSONArray("requests");
+            // 遍历所有【权限请求】，判断当前请求的地址是否在【权限请求】里
+            for (int i = 0, l = requests.size(); i < l; i++) {
+                String request = (String) requests.get(i);
+                if (path.contains(request)) {
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist) {
+                LOG.info("权限校验通过");
+            } else {
+                LOG.warn("权限校验未通过");
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
             return chain.filter(exchange);
         }
     }
