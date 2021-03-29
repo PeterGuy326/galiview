86f6363a95d010ab7bfc35e917d30373538c533c
胡奕舟 <739820174@qq.com>
2021-03-29 20:48:12 +0800

    1. 通用权限管理：前端界面权限拦截，增加路由权限判断

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -533,6 +533,10 @@ export default {
     $.getScript('/ace/assets/js/ace.min.js');
 
     _this.loginUser = Tool.getLoginUser();
+
+    if (!_this.hasResourceRouter(_this.$route.name)) {
+      _this.$router.push("/login");
+    }
   },
   watch: {
     $route: {
@@ -540,6 +544,12 @@ export default {
         // sidebar激活样式方法二
         console.log("---->页面跳转：", val, oldVal);
         let _this = this;
+
+        if (!_this.hasResourceRouter(val.name)) {
+          _this.$router.push("/login");
+          return;
+        }
+
         _this.$nextTick(function(){  //页面加载完成后执行
           _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
         })
@@ -547,6 +557,23 @@ export default {
     }
   },
   methods: {
+    /**
+     * 查找是否有权限
+     * @param router
+     */
+    hasResourceRouter(router) {
+      let _this = this;
+      let resources = Tool.getLoginUser().resources;
+      if (Tool.isEmpty(resources)) {
+        return false;
+      }
+      for (let i = 0; i < resources.length; i++) {
+        if (router === resources[i].page) {
+          return true;
+        }
+      }
+      return false;
+    },
     /**
      * 查找是否有权限
      * @param id
