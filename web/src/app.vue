f340f09953e08e1f624cb152bfa520ff38edea23
胡奕舟 <739820174@qq.com>
2021-03-31 14:33:51 +0800

    1. 网站首页开发，将顶部提取成the-header组件

--- web/src/app.vue
+++ web/src/app.vue
@@ -1,36 +1,6 @@
 <template>
   <div>
-    <header>
-      <div class="collapse bg-dark" id="navbarHeader">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">About</h4>
-              <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-            </div>
-            <div class="col-sm-4 offset-md-1 py-4">
-              <h4 class="text-white">Contact</h4>
-              <ul class="list-unstyled">
-                <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                <li><a href="#" class="text-white">Like on Facebook</a></li>
-                <li><a href="#" class="text-white">Email me</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="navbar navbar-dark bg-dark shadow-sm">
-        <div class="container d-flex justify-content-between">
-          <a href="#" class="navbar-brand d-flex align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
-            <strong>Album</strong>
-          </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
-      </div>
-    </header>
+    <the-header></the-header>
 
     <main role="main">
 
@@ -88,7 +58,7 @@
                     <div class="btn-group">
                       <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                       <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
+                    </div>ß
                     <small class="text-muted">9 mins</small>
                   </div>
                 </div>
@@ -153,7 +123,6 @@
                     </div>
                     <small class="text-muted">9 mins</small>
                   </div>
-                  </div>`
                 </div>
               </div>
             </div>
@@ -189,6 +158,7 @@
             </div>
           </div>
         </div>
+      </div>
 
     </main>
 
@@ -206,7 +176,9 @@
 
 <script>
 
+import TheHeader from "./components/the-header";
 export default {
   name: 'app',
+  components: {TheHeader},
 }
 </script>
