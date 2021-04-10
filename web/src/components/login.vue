ccee2558f7cde778f71ac87dbde4c6d2c32fb5b1
DESKTOP-7D73BLM\胡奕舟 <739820174@qq.com>
2021-04-11 00:22:48 +0800

    1. 会员登录注册功能开发，增加登录框和忘记密码框，并实现登录框、注册框、忘记密码框切换，默认显示登录框

--- web/src/components/login.vue
+++ web/src/components/login.vue
@@ -3,7 +3,43 @@
         <div class="modal-dialog modal-login" role="document">
             <div class="modal-content">
                 <div class="modal-body">
-                    <div class="register-div">
+                    <div class="login-div" v-show="MODAL_STATUS === STATUS_LOGIN">
+                        <h3>登&nbsp;&nbsp;录</h3>
+                        <div class="form-group">
+                            <input v-model="member.mobile" class="form-control" placeholder="手机号">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" type="password" placeholder="密码" v-model="member.passwordOriginal">
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input id="image-code-input" class="form-control" type="text" placeholder="验证码"
+                                       v-model="member.imageCode">
+                                <div class="input-group-addon" id="image-code-addon">
+                                    <img id="image-code" alt="验证码" v-on:click="loadImageCode()"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-primary btn-block submit-button">
+                                登&nbsp;&nbsp;录
+                            </button>
+                        </div>
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" class=" rememberMe" v-model="rememberMe"> 记住密码
+                                </label>
+                                <div class="pull-right">
+                                    <a href="javascript:;" v-on:click="toForgetDiv()">忘记密码</a>&nbsp;
+                                    <a href="javascript:;" v-on:click="toRegisterDiv()">我要注册</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group to-register-div">
+                        </div>
+                    </div>
+                    <div class="register-div" v-show="MODAL_STATUS === STATUS_REGISTER">
                         <h3>注&nbsp;&nbsp;册</h3>
                         <div class="form-group">
                             <input id="register-mobile" v-model="memberRegister.mobile"
@@ -39,7 +75,41 @@
                             </button>
                         </div>
                         <div class="form-group to-login-div">
-                            <a href="javascript:;">我要登录</a>
+                            <a href="javascript:;" v-on:click="toLoginDiv()">我要登录</a>
+                        </div>
+                    </div>
+                    <div class="forget-div" v-show="MODAL_STATUS === STATUS_FORGET">
+                        <h3>忘记密码</h3>
+                        <div class="form-group">
+                            <input id="forget-mobile" v-model="memberForget.mobile"
+                                   class="form-control" placeholder="手机号">
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input id="forget-mobile-code" class="form-control"
+                                       placeholder="手机验证码" v-model="memberForget.code">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary" id="forget-send-code-btn">
+                                        发送验证码
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input id="forget-password" v-model="memberForget.passwordOriginal"
+                                   class="form-control" placeholder="密码" type="password">
+                        </div>
+                        <div class="form-group">
+                            <input id="forget-confirm-password" v-model="memberForget.confirm"
+                                   class="form-control" placeholder="确认密码" type="password">
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-primary btn-block submit-button">
+                                重&nbsp;&nbsp;置
+                            </button>
+                        </div>
+                        <div class="form-group to-login-div">
+                            <a href="javascript:;" v-on:click="toLoginDiv()">我要登录</a>
                         </div>
                     </div>
                 </div>
@@ -54,15 +124,23 @@
     name: 'the-login',
     data: function () {
       return {
+        // 模态框内容切换：登录、注册、忘记密码
+        STATUS_LOGIN: "STATUS_LOGIN",
+        STATUS_REGISTER: "STATUS_REGISTER",
+        STATUS_FORGET: "STATUS_FORGET",
+        MODAL_STATUS: "",
+
+        member: {},
+        memberForget: {},
         memberRegister: {},
 
         rememberMe: true, // 记住密码
-        key: "PWD", // 密码传输加密盐值
         imageCodeToken: ""
       }
     },
     mounted() {
       let _this = this;
+      _this.toLoginDiv();
     },
     methods: {
 
@@ -72,6 +150,20 @@
       openLoginModal() {
         let _this = this;
         $("#login-modal").modal("show");
+      },
+
+      //---------------登录框、注册框、忘记密码框切换-----------------
+      toLoginDiv() {
+        let _this = this;
+        _this.MODAL_STATUS = _this.STATUS_LOGIN
+      },
+      toRegisterDiv() {
+        let _this = this;
+        _this.MODAL_STATUS = _this.STATUS_REGISTER
+      },
+      toForgetDiv() {
+        let _this = this;
+        _this.MODAL_STATUS = _this.STATUS_FORGET
       },
 
       register() {
@@ -109,7 +201,7 @@
         max-width: 400px;
     }
 
-    #login-modal input {
+    #login-modal input:not(.rememberMe) {
         height: 45px;
         font-size: 16px;
     }
