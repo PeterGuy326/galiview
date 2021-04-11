a9a5e27c7362ce13066f5bf5477f5e788328a88b
DESKTOP-7D73BLM\胡奕舟 <739820174@qq.com>
2021-04-11 23:01:43 +0800

    1. 会员登录注册功能开发，完善【记住我】功能 会员登录名是mobile，不是loginName

--- web/src/components/login.vue
+++ web/src/components/login.vue
@@ -149,14 +149,22 @@
        */
       openLoginModal() {
         let _this = this;
-        // 显示登录框时就刷新一次验证码图片
-        _this.loadImageCode();
         $("#login-modal").modal("show");
       },
 
       //---------------登录框、注册框、忘记密码框切换-----------------
       toLoginDiv() {
         let _this = this;
+
+        // 从缓存中获取记住的用户名密码，如果获取不到，说明上一次没有勾选“记住我”
+        let rememberMember = LocalStorage.get(LOCAL_KEY_REMEMBER_MEMBER);
+        if (rememberMember) {
+          _this.member = rememberMember;
+        }
+
+        // 显示登录框时就刷新一次验证码图片
+        _this.loadImageCode();
+
         _this.MODAL_STATUS = _this.STATUS_LOGIN
       },
       toRegisterDiv() {
@@ -188,9 +196,6 @@
       login () {
         let _this = this;
 
-        // 将明文存储到缓存中
-        // let passwordShow = _this.member.password;
-
         // 如果密码是从缓存带出来的，则不需要重新加密
         let md5 = hex_md5(_this.member.password);
         let rememberMember = LocalStorage.get(LOCAL_KEY_REMEMBER_MEMBER) || {};
@@ -210,17 +215,15 @@
             // 判断“记住我”
             if (_this.remember) {
               // 如果勾选记住我，则将用户名密码保存到本地缓存
-              // 原：这里需要保存密码明文，否则登录时又会再加一层密
-              // 新：这里保存密码密文，并保存密文md5，用于检测密码是否被重新输入过
+              // 这里保存密码密文，并保存密文md5，用于检测密码是否被重新输入过
               let md5 = hex_md5(_this.member.password);
               LocalStorage.set(LOCAL_KEY_REMEMBER_MEMBER, {
-                loginName: loginMember.loginName,
-                // password: _this.member.passwordShow,
+                mobile: loginMember.mobile,
                 password: _this.member.password,
                 md5: md5
               });
             } else {
-              // 没有勾选“记住我”时，要把本地缓存清空，否则按照mounted的逻辑，下次打开时会自动显示用户名密码
+              // 没有勾选“记住我”时，要把本地缓存清空，否则下次显示登录框时会自动显示用户名密码
               LocalStorage.set(LOCAL_KEY_REMEMBER_MEMBER, null);
             }
 
