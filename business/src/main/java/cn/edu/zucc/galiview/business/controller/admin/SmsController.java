a8d2e7b2ca58d3c27022950cf695d338e5b3d451
胡奕舟 <739820174@qq.com>
2021-04-17 22:18:13 +0800

    1. 会员登录注册功能开发，短信管理只能查看

--- business/src/main/java/cn/edu/zucc/galiview/business/controller/admin/SmsController.java
+++ business/src/main/java/cn/edu/zucc/galiview/business/controller/admin/SmsController.java
@@ -1,13 +1,14 @@
 package cn.edu.zucc.galiview.business.controller.admin;
 
-import cn.edu.zucc.galiview.server.dto.SmsDto;
 import cn.edu.zucc.galiview.server.dto.PageDto;
 import cn.edu.zucc.galiview.server.dto.ResponseDto;
 import cn.edu.zucc.galiview.server.service.SmsService;
-import cn.edu.zucc.galiview.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -22,42 +23,13 @@ public class SmsController {
     private SmsService smsService;
 
     /**
-    * 列表查询
-    */
+     * 列表查询
+     */
     @PostMapping("/list")
     public ResponseDto list(@RequestBody PageDto pageDto) {
         ResponseDto responseDto = new ResponseDto();
         smsService.list(pageDto);
         responseDto.setContent(pageDto);
-        return responseDto;
-    }
-
-    /**
-    * 保存，id有值时更新，无值时新增
-    */
-    @PostMapping("/save")
-    public ResponseDto save(@RequestBody SmsDto smsDto) {
-        // 保存校验
-        ValidatorUtil.require(smsDto.getMobile(), "手机号");
-        ValidatorUtil.length(smsDto.getMobile(), "手机号", 1, 50);
-        ValidatorUtil.require(smsDto.getCode(), "验证码");
-        ValidatorUtil.require(smsDto.getUse(), "用途");
-        ValidatorUtil.require(smsDto.getAt().toString(), "生成时间");
-        ValidatorUtil.require(smsDto.getStatus(), "用途");
-
-        ResponseDto responseDto = new ResponseDto();
-        smsService.save(smsDto);
-        responseDto.setContent(smsDto);
-        return responseDto;
-    }
-
-    /**
-    * 删除
-    */
-    @DeleteMapping("/delete/{id}")
-    public ResponseDto delete(@PathVariable String id) {
-        ResponseDto responseDto = new ResponseDto();
-        smsService.delete(id);
         return responseDto;
     }
 }
