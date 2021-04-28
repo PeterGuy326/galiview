1adffb7ef4b685f6651a372b049f4f498bfe4be3
胡奕舟 <739820174@qq.com>
2021-04-28 23:22:33 +0800

    1. 控台欢迎页面开发，增加顶部统计

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -1,6 +1,58 @@
 <template>
   <div>
-    <h1>你好，欢迎进入控台</h1>
+    <div class="space-12"></div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="center">
+          <span class="btn btn-app btn-sm btn-light no-hover">
+              <span class="line-height-1 bigger-170 blue"> 518 </span>
+              <br>
+              <span class="line-height-1 smaller-90"> 课程 </span>
+          </span>
+          <span class="btn btn-app btn-sm btn-yellow no-hover">
+              <span class="line-height-1 bigger-170"> 6,321 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 大章 </span>
+          </span>
+
+          <span class="btn btn-app btn-sm btn-pink no-hover">
+              <span class="line-height-1 bigger-170"> 4,182 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 小节 </span>
+          </span>
+
+          <span class="btn btn-app btn-sm btn-success no-hover">
+              <span class="line-height-1 bigger-170"> 5.23 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 会员(万) </span>
+          </span>
+
+          <span class="btn btn-app btn-sm btn-primary no-hover">
+              <span class="line-height-1 bigger-170"> 12.76 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 报名(万) </span>
+          </span>
+
+          <span class="btn btn-app btn-sm btn-grey no-hover">
+              <span class="line-height-1 bigger-170"> 32.96 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 评论(万) </span>
+          </span>
+
+        </div>
+
+        <div class="space-12"></div>
+
+
+      </div>
+
+
+    </div><!-- /.row -->
   </div>
 </template>
 
@@ -9,14 +61,8 @@ export default {
   name: "welcome",
   mounted: function () {
     // sidebar激活样式方法一
-    // this.$parent.activeSidebar("welcome-sidebar")
+    // this.$parent.activeSidebar("welcome-sidebar");
   },
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
-
-<style>
-
-</style>
