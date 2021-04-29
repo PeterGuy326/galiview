e2d953573c06de4b35d7d02bf5371db2fabbc78f
胡奕舟 <739820174@qq.com>
2021-04-29 15:29:51 +0800

    1. BUG修复，时间格式化注意分和秒的边界

--- admin/src/filter/filter.js
+++ admin/src/filter/filter.js
@@ -47,11 +47,11 @@ let formatSecond = (value) => {
     let second = parseInt(value, 10); // 秒
     let minute = 0; // 分
     let hour = 0; // 小时
-    if (second > 60) {
+    if (second >= 60) {
         // 当大于60秒时，才需要做转换
         minute = Math.floor(second / 60);
         second = Math.floor(second % 60);
-        if (minute > 60) {
+        if (minute >= 60) {
             hour = Math.floor(minute / 60);
             minute = Math.floor(minute % 60);
         }
