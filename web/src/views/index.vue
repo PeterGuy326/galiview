d6f5d148f98ff7c67b60b7fc4057fa73ba61d5a1
胡奕舟 <739820174@qq.com>
2021-04-29 15:15:06 +0800

    1. 前端缓存的使用，新上好课不经常变，又经常被访问，适合用缓存

--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -57,11 +57,22 @@ export default {
      */
     listNew() {
       let _this = this;
+
+      // 新上好课不经常变，又经常被访问，适合用缓存
+      // 判断是否有缓存
+      let news = SessionStorage.get("news");
+      if (!Tool.isEmpty(news)) {
+        _this.news = news;
+        return;
+      }
+
       _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/web/course/list-new').then((response)=>{
         console.log("查询新上好课结果：", response);
         let resp = response.data;
         if (resp.success) {
           _this.news = resp.content;
+          // 保存到缓存
+          SessionStorage.set("news", _this.news);
         }
       }).catch((response)=>{
         console.log("error：", response);
