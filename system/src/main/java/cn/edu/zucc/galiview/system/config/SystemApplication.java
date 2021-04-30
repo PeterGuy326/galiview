d79032f7de4b04f261a7b2a1410cb7c6458519e6
胡奕舟 <739820174@qq.com>
2021-04-30 16:41:59 +0800

    1. 将system模块注册中心改为nacos

--- system/src/main/java/cn/edu/zucc/galiview/system/config/SystemApplication.java
+++ system/src/main/java/cn/edu/zucc/galiview/system/config/SystemApplication.java
@@ -6,12 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @ComponentScan("cn.edu.zucc.galiview")
 @MapperScan("cn.edu.zucc.galiview.server.mapper")
 public class SystemApplication {
