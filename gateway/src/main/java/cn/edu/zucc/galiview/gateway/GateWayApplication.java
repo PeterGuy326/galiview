fe2a31589e8a1dcad35a6745db87f7793f05acaf
胡奕舟 <739820174@qq.com>
2021-04-30 16:52:49 +0800

    1. 将gateway, file, business三个模块注册中心改为nacos

--- gateway/src/main/java/cn/edu/zucc/galiview/gateway/GateWayApplication.java
+++ gateway/src/main/java/cn/edu/zucc/galiview/gateway/GateWayApplication.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,7 +13,7 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GateWayApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(GateWayApplication.class);
